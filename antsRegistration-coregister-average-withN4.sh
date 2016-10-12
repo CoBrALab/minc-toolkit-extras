0da3f0b40419412eb50f94890af98b4583448cd8
Gabriel A. Devenyi <gdevenyi@gmail.com>
2016-10-12 16:14:40 -0400

    Much improved iterative N4 script

--- antsRegistration-coregister-average-withN4.sh
+++ antsRegistration-coregister-average-withN4.sh
@@ -1,48 +1,22 @@
 #!/bin/bash
-#set -e
+set -e
 
-rounds=6
+rounds=4
 output=$1
 tmpdir=$(mktemp -d)
 shift 1
 
 for file in "$@"
 do
-    mkdir $tmpdir/$(basename $file)
-    ImageMath 3 $tmpdir/$(basename $file)/trunc.mnc TruncateImageIntensity $file 0 0.995 2048
-    ImageMath 3 $tmpdir/$(basename $file)/hotspots.mnc TruncateImageIntensity $file 0.95 1 2048
-    ThresholdImage 3 $tmpdir/$(basename $file)/hotspots.mnc $tmpdir/$(basename $file)/hotmask_inv.mnc Otsu 1
-    ImageMath 3 $tmpdir/$(basename $file)/hotmask.mnc Neg $tmpdir/$(basename $file)/hotmask_inv.mnc
 
-    bestlinreg_g -clobber -nmi -lsq12 -target_mask $mnimask $tmpdir/$(basename $file)/trunc.mnc $mnimodel $tmpdir/$(basename $file)/to_mni.xfm
-    itk_resample --clobber --invert_transform --labels --like $tmpdir/$(basename $file)/trunc.mnc --transform $tmpdir/$(basename $file)/to_mni.xfm $mnimask $tmpdir/$(basename $file)/brainmask.mnc
-    mincmorph -successive EE $tmpdir/$(basename $file)/brainmask.mnc $tmpdir/$(basename $file)/brainmaskE.mnc
+iterativeN4.sh $file $tmpdir/$(basename $file).N4.mnc
 
-    #cp $tmpdir/$(basename $file)/trunc.mnc $tmpdir/$(basename $file)/input.mnc
-
-    ThresholdImage 3 $tmpdir/$(basename $file)/trunc.mnc $tmpdir/$(basename $file)/otsu.mnc Otsu 1
-    mincdefrag $tmpdir/$(basename $file)/otsu.mnc $tmpdir/$(basename $file)/otsu_defrag.mnc 1 6
-    autocrop -isoexpand 50mm $tmpdir/$(basename $file)/otsu_defrag.mnc $tmpdir/$(basename $file)/otsu_expanded.mnc
-    itk_morph --exp 'D[25] E[25]' $tmpdir/$(basename $file)/otsu_expanded.mnc $tmpdir/$(basename $file)/otsu_expanded_closed.mnc
-    itk_resample --clobber --labels --like $tmpdir/$(basename $file)/otsu.mnc $tmpdir/$(basename $file)/otsu_expanded_closed.mnc $tmpdir/$(basename $file)/headmask.mnc
-
-    ImageMath 3 $tmpdir/$(basename $file)/brainmaskE_novent.mnc m $tmpdir/$(basename $file)/brainmaskE.mnc $tmpdir/$(basename $file)/otsu.mnc
-    ImageMath 3 $tmpdir/$(basename $file)/brainmaskE_novent_nohot.mnc m $tmpdir/$(basename $file)/brainmaskE_novent.mnc $tmpdir/$(basename $file)/hotmask.mnc
-
-    N4BiasFieldCorrection -d 3 -i $tmpdir/$(basename $file)/trunc.mnc -w $tmpdir/$(basename $file)/brainmaskE_novent_nohot.mnc -b [400] -c [200x200x200x200x200,0.0] -v -r 1 -x $tmpdir/$(basename $file)/headmask.mnc -o $tmpdir/$(basename $file)/input.mnc
-
-    # for i in {1..3}
-    # do
-    #     itk_minc_nonlocal_filter --clobber --beta 0.5 --anlm $tmpdir/$(basename $file)/input.mnc $tmpdir/$(basename $file)/denoise.mnc
-    #     ThresholdImage 3 $tmpdir/$(basename $file)/denoise.mnc $tmpdir/$(basename $file)/kmeans.mnc Kmeans 2 $tmpdir/$(basename $file)/brainmaskE.mnc
-    #     minclookup -clobber -discrete -lut_string "3 1" $tmpdir/$(basename $file)/kmeans.mnc $tmpdir/$(basename $file)/wm.mnc
-    #     N4BiasFieldCorrection -d 3 -i $tmpdir/$(basename $file)/trunc.mnc -w $tmpdir/$(basename $file)/wm.mnc -b [400] -c [200x200x200x200x200,0.0] -v -r 1 -x $tmpdir/$(basename $file)/headmask.mnc -o $tmpdir/$(basename $file)/input.mnc
-    # done
-    ImageMath 3 $tmpdir/$(basename $file).N4.mnc m $tmpdir/$(basename $file)/input.mnc $tmpdir/$(basename $file)/headmask.mnc
-    #cp $tmpdir/$(basename $file)/input.mnc $tmpdir/$(basename $file).N4.mnc
 done
 
+set +e
+
 mincbigaverage --robust --sdfile $tmpdir/sd0.mnc $tmpdir/*.N4.mnc $tmpdir/average0.mnc
+
 target=$tmpdir/average0.mnc
 
 iters="2000"
@@ -53,12 +27,13 @@ for ((n=1;n<$rounds;n++))
 do
   for file in $tmpdir/*.N4.mnc
   do
-    antsRegistration --dimensionality 3 --float 0 --collapse-output-transforms 1 --minc --verbose --interpolation BSpline \
+    antsRegistration --dimensionality 3 --float 0 --collapse-output-transforms 1 --minc --verbose --interpolation Linear \
                 --output [$tmpdir/$(basename $file)-$(basename $target),$tmpdir/$(basename $file)-$(basename $target)] \
                 --transform Rigid[0.1] --metric GC[$target,$file,1] \
-                --convergence [$iters,1e-6,10] --shrink-factors $shrink --smoothing-sigmas $sigma
+                --convergence [$iters,1e-7,10] --shrink-factors $shrink --smoothing-sigmas $sigma
   done
   mincaverage -normalize -sdfile $tmpdir/sd$n.mnc $tmpdir/*-$(basename $target) $tmpdir/average$n.mnc
+
   target=$tmpdir/average$n.mnc
   iters+="x2000"
   shrink+="x$(expr $rounds - $n - 1)"
@@ -66,5 +41,5 @@ do
 done
 
 #cp $target $output
-mincmath -clamp -const2 0 $(mincstats -quiet -max $target) $target $output
+mincmath -clamp -const2 0 $(mincstats -quiet -max $tmpdir/average0.mnc) $target $output
 rm -r $tmpdir
---
+++ iterativeN4.sh
@@ -0,0 +1,153 @@
+#!/bin/bash
+set -e
+BEASTMODEL_DIR="${QUARANTINE_PATH}/resources/mni_icbm152_nlin_sym_09c_minc2"
+BEASTMODEL_NAME="mni_icbm152_t1_tal_nlin_sym_09c"
+BEASTLIBRARY_DIR="${QUARANTINE_PATH}/resources/BEaST_libraries/combined"
+REGISTRATIONMODEL="${QUARANTINE_PATH}/resources/mni_icbm152_nlin_sym_09c_minc2/mni_icbm152_t1_tal_nlin_sym_09c.mnc"
+REGISTRATIONBRAINMASK="${QUARANTINE_PATH}/resources/mni_icbm152_nlin_sym_09c_minc2/mni_icbm152_t1_tal_nlin_sym_09c_mask.mnc"
+REGISTRATIONHEADMASK="${QUARANTINE_PATH}/resources/mni_icbm152_nlin_sym_09c_minc2/mni_icbm152_t1_tal_nlin_sym_09c_headmask.mnc"
+REGISTRATIONWMMASK="${QUARANTINE_PATH}/resources/mni_icbm152_nlin_sym_09c_minc2/mni_icbm152_wm_tal_nlin_sym_09c.mnc"
+REGISTRATIONGMMASK="${QUARANTINE_PATH}/resources/mni_icbm152_nlin_sym_09c_minc2/mni_icbm152_gm_tal_nlin_sym_09c.mnc"
+WMPRIOR="${QUARANTINE_PATH}/resources/mni_icbm152_nlin_sym_09c_minc2/mni_icbm152_wm_tal_nlin_sym_09c.mnc"
+GMPRIOR="${QUARANTINE_PATH}/resources/mni_icbm152_nlin_sym_09c_minc2/mni_icbm152_gm_tal_nlin_sym_09c.mnc"
+CSFPRIOR="${QUARANTINE_PATH}/resources/mni_icbm152_nlin_sym_09c_minc2/mni_icbm152_csf_tal_nlin_sym_09c.mnc"
+
+tmpdir=$(mktemp -d)
+
+#Generate a whole-image mask
+minccalc -clobber -unsigned -byte -expression 'A[0]?1:1' $1 $tmpdir/initmask.mnc
+
+#Bootstrap mask for first intensity normalization
+cp ${REGISTRATIONBRAINMASK} $tmpdir/beastmask0.mnc
+
+#Very lightly truncate image intensity and re-normalize
+ImageMath 3 $tmpdir/trunc.mnc TruncateImageIntensity $1 0.025 0.995 256
+ImageMath 3 $tmpdir/truncnorm0.mnc Normalize $tmpdir/trunc.mnc
+ImageMath 3 $tmpdir/trunc.mnc m $tmpdir/truncnorm0.mnc 1000
+
+#Correct entire image domain
+N4BiasFieldCorrection -d 3 -s 4 --verbose -x $tmpdir/initmask.mnc \
+-b [200] -c [300x300x300x300,0.0] --histogram-sharpening [0.05,0.01,200] -i $tmpdir/trunc.mnc -o $tmpdir/precorrect.mnc
+
+#Normalize and rescale intensity
+ImageMath 3 $tmpdir/norm0.mnc Normalize $tmpdir/precorrect.mnc
+ImageMath 3 $tmpdir/precorrect.mnc m $tmpdir/norm0.mnc 1000
+
+#First iteration
+n=0
+
+#Denoise for registration and beast
+minc_anlm --clobber --rician $tmpdir/precorrect.mnc $tmpdir/denoise${n}.mnc
+
+#Register to MNI space
+bestlinreg_g -nmi -lsq12 -target_mask ${REGISTRATIONBRAINMASK} $tmpdir/denoise${n}.mnc ${REGISTRATIONMODEL} $tmpdir/0_${n}.xfm $tmpdir/mni${n}.mnc -clobber
+
+#Intensity normalize
+volume_pol --order 1 --min 0 --max 100 --noclamp $tmpdir/mni${n}.mnc ${REGISTRATIONMODEL} --source_mask $tmpdir/beastmask0.mnc --target_mask ${REGISTRATIONBRAINMASK} --clobber $tmpdir/mni${n}.norm.mnc
+
+#Run a quick beast to get a brain mask
+mincbeast -clobber -verbose -fill -median -same_res -flip -conf /opt/quarantine/resources/BEaST_libraries/combined/default.4mm.conf /opt/quarantine/resources/BEaST_libraries/combined $tmpdir/mni${n}.norm.mnc $tmpdir/beastmask${n}.mnc
+
+#Resample beast mask and MNI mask to native space
+itk_resample --clobber --labels --like $tmpdir/denoise${n}.mnc --invert_transform --transform $tmpdir/0_${n}.xfm ${REGISTRATIONBRAINMASK} $tmpdir/mnimask${n}.mnc
+itk_resample --clobber --labels --like $tmpdir/denoise${n}.mnc --invert_transform --transform $tmpdir/0_${n}.xfm $tmpdir/beastmask${n}.mnc $tmpdir/bmask${n}.mnc
+#Combine the masks because sometimes beast misses badly biased cerebellum
+mincmath -clobber -or $tmpdir/mnimask${n}.mnc $tmpdir/bmask${n}.mnc $tmpdir/mask${n}.mnc
+
+#Expand the mask a bit
+mincmorph -3D26 -successive DD $tmpdir/mask${n}.mnc $tmpdir/mask${n}_D.mnc -clobber
+
+#Do first round of masked bias field correction, use brain mask as weight
+N4BiasFieldCorrection -d 3 -s 4 --verbose -w $tmpdir/mask${n}_D.mnc -x $tmpdir/initmask.mnc \
+-b [200] -c [300x300x300x300,0.0] --histogram-sharpening [0.05,0.01,200] -i $tmpdir/trunc.mnc -o $tmpdir/round${n}.mnc
+
+#Normalize and rescale intensity
+ImageMath 3 $tmpdir/norm${n}.mnc Normalize $tmpdir/round${n}.mnc
+ImageMath 3 $tmpdir/round${n}.mnc m $tmpdir/norm${n}.mnc 1000
+
+n=1
+
+#Denoise new output
+minc_anlm --clobber --rician $tmpdir/round$((${n} - 1)).mnc $tmpdir/denoise${n}.mnc
+
+#Register to MNI space
+bestlinreg_g -nmi -lsq12 -target_mask ${REGISTRATIONBRAINMASK} $tmpdir/denoise${n}.mnc ${REGISTRATIONMODEL} $tmpdir/0_${n}.xfm $tmpdir/mni${n}.mnc -clobber
+
+antsRegistration --dimensionality 3 --float 0 --collapse-output-transforms 1 -a 1 --verbose --minc \
+  --output $tmpdir/nonlin${n} \
+  --initial-moving-transform $tmpdir/0_${n}.xfm \
+  --transform BSplineSyN[0.1,26,0,3] --metric MI[$tmpdir/denoise${n}.mnc,${REGISTRATIONMODEL},1,32] --convergence [100x70x50x0,1e-6,10] --shrink-factors 8x4x2x1 --smoothing-sigmas 3x2x1x0vox
+
+#Intensity normalize
+volume_pol --order 1 --min 0 --max 100 --noclamp $tmpdir/mni${n}.mnc ${REGISTRATIONMODEL} --source_mask $tmpdir/beastmask$((${n} - 1)).mnc --target_mask ${REGISTRATIONBRAINMASK} --clobber $tmpdir/mni${n}.norm.mnc
+
+#Quick Beast
+mincbeast -clobber -verbose -fill -median -same_res -flip -conf /opt/quarantine/resources/BEaST_libraries/combined/default.4mm.conf /opt/quarantine/resources/BEaST_libraries/combined $tmpdir/mni${n}.norm.mnc $tmpdir/beastmask${n}.mnc
+
+#Resample and join brain masks
+itk_resample --clobber --labels --like $tmpdir/denoise${n}.mnc --invert_transform --transform $tmpdir/0_${n}.xfm ${REGISTRATIONBRAINMASK} $tmpdir/mnimask${n}.mnc
+itk_resample --clobber --labels --like $tmpdir/denoise${n}.mnc --invert_transform --transform $tmpdir/0_${n}.xfm $tmpdir/beastmask${n}.mnc $tmpdir/bmask${n}.mnc
+mincmath -clobber -or $tmpdir/mnimask${n}.mnc $tmpdir/bmask${n}.mnc $tmpdir/mask${n}.mnc
+
+#Expand the mask a bit
+mincmorph -3D26 -successive DD $tmpdir/mask${n}.mnc $tmpdir/mask${n}_D.mnc -clobber
+
+#Resample MNI Priors to Native space for classification
+antsApplyTransforms -i ${WMPRIOR} -t $tmpdir/nonlin${n}.xfm -r $tmpdir/denoise${n}.mnc -o $tmpdir/round${n}SegmentationPrior3.mnc -d 3 -n Linear
+antsApplyTransforms -i ${GMPRIOR} -t $tmpdir/nonlin${n}.xfm -r $tmpdir/denoise${n}.mnc -o $tmpdir/round${n}SegmentationPrior2.mnc -d 3 -n Linear
+antsApplyTransforms -i ${CSFPRIOR} -t $tmpdir/nonlin${n}.xfm -r $tmpdir/denoise${n}.mnc -o $tmpdir/round${n}SegmentationPrior1.mnc -d 3 -n Linear
+
+#itk_resample --clobber --order 1 --like $tmpdir/denoise${n}.mnc --invert_transform --transform $tmpdir/0_${n}.xfm ${WMPRIOR} $tmpdir/round${n}SegmentationPrior3.mnc
+#itk_resample --clobber --order 1 --like $tmpdir/denoise${n}.mnc --invert_transform --transform $tmpdir/0_${n}.xfm ${GMPRIOR} $tmpdir/round${n}SegmentationPrior2.mnc
+#itk_resample --clobber --order 1 --like $tmpdir/denoise${n}.mnc --invert_transform --transform $tmpdir/0_${n}.xfm ${CSFPRIOR} $tmpdir/round${n}SegmentationPrior1.mnc
+
+#Do an initial classification using the MNI priors, remove outliers
+Atropos -d 3 -x $tmpdir/mask${n}_D.mnc -c [5,0.0] -a $tmpdir/round$((${n} - 1)).mnc --verbose 1 -i  PriorProbabilityImages[3,$tmpdir/round${n}SegmentationPrior%d.mnc,0.25] -k Gaussian -m [0.1,1x1x1] \
+-o [$tmpdir/classify${n}.mnc,$tmpdir/round${n}SegmentationPosteriors%d.mnc] -r 1 -p Socrates[0] -w GrubbsRosner
+
+#Combine GM and WM probably images into a N4 mask,
+ImageMath 3 $tmpdir/weight${n}.mnc PureTissueN4WeightMask $tmpdir/round${n}SegmentationPosteriors2.mnc $tmpdir/round${n}SegmentationPosteriors3.mnc
+
+#Perform bias field correction with weight mask
+N4BiasFieldCorrection -d 3 -s 2 --verbose -w $tmpdir/weight${n}.mnc -x $tmpdir/initmask.mnc \
+-b [200] -c [300x300x300x300,0.0] --histogram-sharpening [0.05,0.01,200] -i $tmpdir/trunc.mnc -o $tmpdir/round${n}.mnc
+
+#Normalize intensity
+ImageMath 3 $tmpdir/norm${n}.mnc Normalize $tmpdir/round${n}.mnc
+ImageMath 3 $tmpdir/round${n}.mnc m $tmpdir/norm${n}.mnc 1000
+
+#Repeat until happy
+for (( n = 2; n <= 3; n++ ))
+do
+
+  minc_anlm --clobber --rician $tmpdir/round$((${n} - 1)).mnc $tmpdir/denoise${n}.mnc
+
+  bestlinreg_g -nmi -lsq12 -target_mask ${REGISTRATIONBRAINMASK} $tmpdir/denoise${n}.mnc ${REGISTRATIONMODEL} $tmpdir/0_${n}.xfm $tmpdir/mni${n}.mnc -clobber
+
+  volume_pol --order 1 --min 0 --max 100 --noclamp $tmpdir/mni${n}.mnc ${REGISTRATIONMODEL} --source_mask $tmpdir/beastmask$((${n} - 1)).mnc --target_mask ${REGISTRATIONBRAINMASK} --clobber $tmpdir/mni${n}.norm.mnc
+
+  mincbeast -clobber -verbose -fill -median -same_res -flip -conf /opt/quarantine/resources/BEaST_libraries/combined/default.4mm.conf /opt/quarantine/resources/BEaST_libraries/combined $tmpdir/mni${n}.norm.mnc $tmpdir/beastmask${n}.mnc
+
+  itk_resample --clobber --labels --like $tmpdir/denoise${n}.mnc --invert_transform --transform $tmpdir/0_${n}.xfm ${REGISTRATIONBRAINMASK} $tmpdir/mnimask${n}.mnc
+  itk_resample --clobber --labels --like $tmpdir/denoise${n}.mnc --invert_transform --transform $tmpdir/0_${n}.xfm $tmpdir/beastmask${n}.mnc $tmpdir/bmask${n}.mnc
+  mincmath -or $tmpdir/mnimask${n}.mnc $tmpdir/bmask${n}.mnc $tmpdir/mask${n}.mnc -clobber
+
+  mincmorph -3D26 -successive DD $tmpdir/mask${n}.mnc $tmpdir/mask${n}_D.mnc -clobber
+
+  Atropos -d 3 -x $tmpdir/mask${n}_D.mnc -c [5,0.0] -a $tmpdir/round$((${n} - 1)).mnc --verbose 1 -i PriorProbabilityImages[3,$tmpdir/round$((${n} -1 ))SegmentationPosteriors%d.mnc,0.25] \
+  -k Gaussian -m [0.1,1x1x1] -o [$tmpdir/classify${n}.mnc,$tmpdir/round${n}SegmentationPosteriors%d.mnc] -r 1 -p Socrates[1]
+
+  ImageMath 3 $tmpdir/weight${n}.mnc PureTissueN4WeightMask $tmpdir/round${n}SegmentationPosteriors2.mnc $tmpdir/round${n}SegmentationPosteriors3.mnc
+
+  N4BiasFieldCorrection -d 3 -s 2 --verbose -w $tmpdir/weight${n}.mnc -x $tmpdir/initmask.mnc \
+  -b [200] -c [300x300x300x300,0.0] --histogram-sharpening [0.05,0.01,200] -i $tmpdir/trunc.mnc -o $tmpdir/round${n}.mnc
+
+  ImageMath 3 $tmpdir/norm${n}.mnc Normalize $tmpdir/round${n}.mnc
+  ImageMath 3 $tmpdir/round${n}.mnc m $tmpdir/norm${n}.mnc 1000
+
+done
+
+mincstats -quiet -histogram $(dirname $2)/$(basename $2 .mnc).hist -mask $tmpdir/mask3_D.mnc -mask_binvalue 1 $tmpdir/round3.mnc
+
+cp -f $tmpdir/round3.mnc $2
+rm -rf $tmpdir
