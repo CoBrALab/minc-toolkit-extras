e130614cfc8c9bf3d31e492524a7beffb4c52521
Gabriel A. Devenyi <gdevenyi@gmail.com>
2020-12-08 12:18:22 -0500

    Add smooth_downsample.sh for isotropizing images

---
+++ smooth_downsample.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+#Isotropize, and normalize intensity range, this is the file that will be processed in the pipeline
+#Need smoothing for downsampling to avoid aliasing
+#Ideas stolen from https://discourse.itk.org/t/resampling-to-isotropic-signal-processing-theory/1403
+
+set -euo pipefail
+
+if [ "$#" -ne 3 ]; then
+  echo "You must enter exactly 3 command line arguments: isostep input output"
+fi
+
+tmpdir=$(mktemp -d)
+
+isostep=$1
+input=$2
+output=$3
+
+inputres=$(python -c "print('\n'.join([str(abs(x)) for x in [float(x) for x in \"$(PrintHeader ${input} 1)\".split(\"x\")]]))")
+
+blurs=""
+for dim in ${inputres}; do
+  if [[ $(python -c "print(${dim}>(${isostep}-1e-6))") == True ]]; then
+    blurs+=1e-12x
+  else
+    blurs+=$(python -c "import math; print(math.sqrt((${isostep}**2.0 - ${dim}**2.0)/(2.0*math.sqrt(2.0*math.log(2.0)))**2.0))")x
+  fi
+done
+
+SmoothImage 3 ${input} "${blurs%?}" ${tmpdir}/smoothed.nii.gz 1 0
+ResampleImage 3 ${tmpdir}/smoothed.nii.gz ${output} ${isostep}x${isostep}x${isostep} 0 4
+
+rm -rf ${tmpdir}
