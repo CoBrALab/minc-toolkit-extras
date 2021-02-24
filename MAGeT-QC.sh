3c9d9641d975d049bf8bf69e665ae7649c9e5ad4
Gabriel A. Devenyi <gdevenyi@gmail.com>
2021-02-24 13:44:31 -0500

    Add option for none mask

--- MAGeT-QC.sh
+++ MAGeT-QC.sh
@@ -13,45 +13,58 @@ label=$2
 output=$3
 tmpdir=$(mktemp -d)
 
-mincresample $(mincbbox -mincresample $label) $label $tmpdir/label-crop.mnc
-minccalc -expression "1" $tmpdir/label-crop.mnc $tmpdir/bounding.mnc
+if [[ ${label} == "none" ]]; then
+    ThresholdImage 3 ${image} ${tmpdir}/bgmask.mnc 1e-12 Inf 1 0
+    ThresholdImage 3 ${image} ${tmpdir}/bgmask.mnc Otsu 4 ${tmpdir}/bgmask.mnc
+    ThresholdImage 3 ${tmpdir}/bgmask.mnc ${tmpdir}/bgmask.mnc 2 Inf 1 0
+    mincresample -keep -near -like ${image} ${tmpdir}/bgmask.mnc ${tmpdir}/resample.mnc
+    mincresample $(mincbbox -mincresample ${tmpdir}/resample.mnc) ${tmpdir}/resample.mnc ${tmpdir}/label-crop.mnc
+    minccalc -expression "1" ${tmpdir}/label-crop.mnc ${tmpdir}/bounding.mnc
+else
+    mincresample $(mincbbox -mincresample ${label}) ${label} ${tmpdir}/label-crop.mnc
+    minccalc -expression "1" ${tmpdir}/label-crop.mnc ${tmpdir}/bounding.mnc
+    #Labelled layers
+    create_verify_image -align_com -range_floor 0 ${tmpdir}/$(basename ${image} .mnc)_t.rgb \
+        -width 1920 -autocols 10 -autocol_planes t \
+        -bounding_volume ${tmpdir}/bounding.mnc \
+        -row ${image} color:gray \
+        volume_overlay:${label}:0.3
+
+    create_verify_image -align_com -range_floor 0 ${tmpdir}/$(basename ${image} .mnc)_s.rgb \
+        -width 1920 -autocols 10 -autocol_planes s \
+        -bounding_volume ${tmpdir}/bounding.mnc \
+        -row ${image} color:gray \
+        volume_overlay:${label}:0.3
+
+    create_verify_image -align_com -range_floor 0 ${tmpdir}/$(basename ${image} .mnc)_c.rgb \
+        -width 1920 -autocols 10 -autocol_planes c \
+        -bounding_volume ${tmpdir}/bounding.mnc \
+        -row ${image} color:gray \
+        volume_overlay:${label}:0.3
+fi
+
+
+
+
 
 #Trasverse
-create_verify_image -align_com -range_floor 0 $tmpdir/$(basename $image .mnc)_t.rgb \
--width 1920 -autocols 10 -autocol_planes t \
--bounding_volume $tmpdir/bounding.mnc \
--row $image color:gray \
-volume_overlay:$label:0.3
-
-create_verify_image -align_com -range_floor 0 $tmpdir/$(basename $image .mnc)_t2.rgb \
--width 1920 -autocols 10 -autocol_planes t \
--bounding_volume $tmpdir/bounding.mnc \
--row $image color:gray
+create_verify_image -align_com -range_floor 0 ${tmpdir}/$(basename ${image} .mnc)_t2.rgb \
+    -width 1920 -autocols 10 -autocol_planes t \
+    -bounding_volume ${tmpdir}/bounding.mnc \
+    -row ${image} color:gray
 
 #Saggital
-create_verify_image -align_com -range_floor 0 $tmpdir/$(basename $image .mnc)_s.rgb \
--width 1920 -autocols 10 -autocol_planes s \
--bounding_volume $tmpdir/bounding.mnc \
--row $image color:gray \
-volume_overlay:$label:0.3
-
-create_verify_image -align_com -range_floor 0 $tmpdir/$(basename $image .mnc)_s2.rgb \
--width 1920 -autocols 10 -autocol_planes s \
--bounding_volume $tmpdir/bounding.mnc \
--row $image color:gray
+create_verify_image -align_com -range_floor 0 ${tmpdir}/$(basename ${image} .mnc)_s2.rgb \
+    -width 1920 -autocols 10 -autocol_planes s \
+    -bounding_volume ${tmpdir}/bounding.mnc \
+    -row ${image} color:gray
 
 #Coronal
-create_verify_image -align_com -range_floor 0 $tmpdir/$(basename $image .mnc)_c.rgb \
--width 1920 -autocols 10 -autocol_planes c \
--bounding_volume $tmpdir/bounding.mnc \
--row $image color:gray \
-volume_overlay:$label:0.3
+create_verify_image -align_com -range_floor 0 ${tmpdir}/$(basename ${image} .mnc)_c2.rgb \
+    -width 1920 -autocols 10 -autocol_planes c \
+    -bounding_volume ${tmpdir}/bounding.mnc \
+    -row ${image} color:gray
 
-create_verify_image -align_com -range_floor 0 $tmpdir/$(basename $image .mnc)_c2.rgb \
--width 1920 -autocols 10 -autocol_planes c \
--bounding_volume $tmpdir/bounding.mnc \
--row $image color:gray
+convert -background black -strip -interlace Plane -sampling-factor 4:2:0 -quality "85%"  -append -trim ${tmpdir}/*.rgb $output
 
-convert -background black -strip -interlace Plane -sampling-factor 4:2:0 -quality "85%"  -append -trim $tmpdir/*.rgb $output
-
-rm -rf $tmpdir
+rm -rf ${tmpdir}
